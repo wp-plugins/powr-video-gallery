55f6b89a485b51dd14724e641223f473702d95cd
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-11 06:49:04 +0000

    App and readme updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-video-gallery/trunk@987008 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-video-gallery.php
+++ powr-video-gallery.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Video Gallery
-     * @version 1.0
+     * @version 1.1
      */
     /*
     Plugin Name: POWr Video Gallery
     Plugin URI: http://www.powr.io
     Description: Showcase YouTube and Vimeo videos in a beautiful gallery! Add the widget to your theme, or create a Video Gallery on ANY page or post by using the shortcode [powr-video-gallery]. Then, simply visit your site and click the settings icon to customize your Video Gallery right in the page. Many more plugins & tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.0
+    Version: 1.1
     Author URI: http://www.powr.io
     */
 
@@ -42,7 +42,7 @@
           if(powr_local_mode()){//Determine JS url:
             $js_url = '//localhost:3000/powr_local.js';
           }else{
-            $js_url = '//powr.io/powr.js';
+            $js_url = '//www.powr.io/powr.js';
           }
           ?>
           <script>
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: http://powr.io/
 Tags: video gallery, youtube gallery, vimeo gallery, vine gallery, video gallery widget, video gallery plugin, mobile responsive
 Requires at least: 3.0
 Tested up to: 4.0
-Stable tag: 1.0
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -74,7 +74,16 @@ Want a more detailed tutorial? Visit [How to add Video Gallery to your Wordpress
 = 1.0 =
 First released version.
 
+= 1.1 =
+Improved SSL Support
+
+
 
 == Changelog ==
 = 1.0 =
 * First released version
+
+= 1.1 =
+* Improved SSL Support
+
+
