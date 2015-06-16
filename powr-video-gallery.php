83a7ed41d65e1ff56c775ebb5351c511094932a1
powr <powr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-16 16:59:51 +0000

    Updates
    git-svn-id: https://plugins.svn.wordpress.org/powr-video-gallery/trunk@1182006 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- powr-video-gallery.php
+++ powr-video-gallery.php
@@ -1,14 +1,14 @@
 <?php
     /**
      * @package POWr Video Gallery
-     * @version 1.4.006
+     * @version 1.4.007
      */
     /*
     Plugin Name: POWr Video Gallery
     Plugin URI: http://www.powr.io
     Description: Showcase YouTube and Vimeo videos in a beautiful gallery!  Drop the widget anywhere in your theme. Or use the POWr icon in your WP text editor to add to a page or post. Edit on your live page by clicking the settings icon. More plugins and tutorials at POWr.io.
     Author: POWr.io
-    Version: 1.4.006
+    Version: 1.4.007
     Author URI: http://www.powr.io
     */
 
